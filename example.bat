ada3a0ec7580baa8e66f20f93efbb85871b3c6f5
Nam Hyeon Uk <jwvg0425@naver.com>
2017-04-13 13:32:06 +0900

    Update example.bat

--- example.bat
+++ example.bat
@@ -11,7 +11,7 @@ rd /s /q "%nowdir%\Binaries"
 rd /s /q "%nowdir%\Intermediate"
 rd /s /q "%nowdir%\Saved"
 
-if exist "%nowdir%\Source\"("%enginepath%\Binaries\DotNet\UnrealBuildTool.exe" -projectfiles -project="%nowdir%\%projectname%.uproject" -game -rocket -progress)
+if exist "%nowdir%\Source" ("%enginepath%\Binaries\DotNet\UnrealBuildTool.exe" -projectfiles -project="%nowdir%\%projectname%.uproject" -game -rocket -progress)
 if exist "%nowdir%\%projectname%.sln" ("%msbuild%" "%nowdir%\%projectname%.sln" /t:build /p:Configuration="Development Editor";Platform=Win64;verbosity=diagnostic)
 
 "%enginepath%\Build\BatchFiles\RunUAT.bat" BuildCookRun -project="%nowdir%\%projectname%.uproject" -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%outputdir%
